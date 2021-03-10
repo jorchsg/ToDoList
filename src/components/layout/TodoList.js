18653e3c1afb3547a6b475032df46da36a796a17
Jorge Sandoval <sandoval.jorge.407@gmail.com>
2021-03-10 10:29:19 -0800

    display ui message for empty tasks

--- src/components/layout/TodoList.js
+++ src/components/layout/TodoList.js
@@ -4,17 +4,23 @@ import TodoListItem from './TodoListItem';
 
 const TodoList = () => {
 
-    const { todos } = useContext(TodoContext)
-    console.log(todos);
+    const { todos } = useContext(TodoContext);
 
     return (
         <>
             <h2>Todo List Items</h2>
-            <ul className="list-group-item list-group-flush bg-light">
-                {todos.map((todo, index) => (
-                    <TodoListItem key={todo.id} todo={todo} index={index} />
-                ))}
-            </ul>
+            {
+                todos.length === 0 ? (
+                    <li className="list-group-item">Not Tasks Yet, Add New One</li>
+                ) : (
+                    <ul className="list-group-item list-group-flush bg-light">
+                        {todos.map((todo, index) => (
+                            <TodoListItem key={todo.id} todo={todo} index={index} />
+                        ))}
+                    </ul>
+                )
+            }
+
         </>
     )
 }
